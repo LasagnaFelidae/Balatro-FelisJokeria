cf6d2a8eefb7042ea6b043699255500ed7a59790
LasagnaFelidae <87911854+LasagnaFelidae@users.noreply.github.com>
2026-01-14 00:52:38 +0100

    Add files via upload

---
+++ main.lua
@@ -0,0 +1,9 @@
+FELIJO = SMODS.current_mod
+
+
+assert(SMODS.load_file("./modules/atlasses.lua"))()
+assert(SMODS.load_file("./modules/content/letterjokers.lua"))()
+assert(SMODS.load_file("./func/garf_dict.lua"))()
+
+
+
